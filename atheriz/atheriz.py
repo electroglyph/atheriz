f705325656f82855ddf50595db136c4f25e29e84
anon <admin@termindoid.com>
2026-08-04 19:09:42 -0700

    misc

--- atheriz/atheriz.py
+++ atheriz/atheriz.py
@@ -6,7 +6,6 @@ import signal
 import time
 from pathlib import Path
 import uvicorn
-from fastapi import FastAPI
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
