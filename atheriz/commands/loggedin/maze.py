d6075c51325cc0475d570d0bc4114983fcaf6742
anon <admin@termindoid.com>
2026-08-19 02:19:41 -0700

    make square mazes

--- atheriz/commands/loggedin/maze.py
+++ atheriz/commands/loggedin/maze.py
@@ -29,8 +29,8 @@ class MazeCommand(Command):
     # pyrefly: ignore
     def run(self, caller: Object, args):
         nh = get_node_handler()
-        width = 50
-        height = 20
+        width = 30
+        height = 30
         # map returned is a rectangular outline around grid, so actual map size returned is +2
         start = time.time()
         map1, grid1 = gen_map_and_grid(width, height, "maze1")
